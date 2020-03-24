f6b265685536b7be432e00cd8d7c3963476d855f
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-24 00:37:58 -0500

    update with latest changes

---
+++ auth/auth-router.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+// const restricted = require('../middleware/restricted');
+const usersModel = require('../users/users-model');
+const secrets = require('../config/secrets');
+
+const router = express.Router();
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const saved = await usersModel.add(req.body);
+    const token = generateToken(saved);
+    const { id, email } = saved;
+    res.status(201).json({ id, email, token });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await usersModel.findBy({ email }).first();
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (user && passwordValid) {
+      const token = generateToken(user);
+      const { id, email } = user;
+      res.status(200).json({ id, email, token });
+    } else {
+      res.status(401).json({ message: 'Invalid Credentials' });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// ---------------------- Generate Token ---------------------- //
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // standard claim = sub
+    email: user.email,
+  };
+  const options = {
+    expiresIn: '7d',
+  };
+  return jwt.sign(payload, secrets.jwt, options);
+}
+
+module.exports = router;
